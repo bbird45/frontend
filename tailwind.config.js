f6ad22eaf9fa9dad08e2eafc10ceada1baba1d51
Bird Tinapat <bbird0662@gmail.com>
2025-01-19 18:50:13 +0700

    [update t]

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      screens: {
-        'sm': '640px', // สำหรับมือถือขนาดเล็ก
-        'md': '768px', // สำหรับแท็บเล็ต
-        'lg': '1024px', // สำหรับหน้าจอขนาดกลาง
-        'xl': '1280px', // สำหรับหน้าจอขนาดใหญ่
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
+
